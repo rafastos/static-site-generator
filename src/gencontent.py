5f5767f77a2eed25a5294457a61ff568637f1c40
Rafael S Lima <rafaelkra2@gmail.com>
2025-10-27 23:41:17 -0300

    add: file to generate page from .md recursively

---
+++ src/gencontent.py
@@ -0,0 +1,38 @@
+import os
+from markdown_blocks import markdown_to_html_node
+
+def extract_title(markdown):
+    lines = markdown.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:].strip()
+    raise ValueError("No title found in markdown")
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
+    with open(from_path, "r", encoding="utf-8") as f:
+        markdown = f.read()
+    
+    with open(template_path, "r", encoding="utf-8") as f:
+        template = f.read()
+
+    html_content = markdown_to_html_node(markdown).to_html()
+    title = extract_title(markdown)
+    final_html = template.replace("{{ Title }}", title).replace("{{ Content }}", html_content)
+
+    dest_dir = os.path.dirname(dest_path)
+    if dest_dir and not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+
+    with open(dest_path, "w", encoding="utf-8") as f:
+        f.write(final_html)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for entry in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, entry)
+        dest_path = os.path.join(dest_dir_path, entry.replace(".md", ".html"))
+        if os.path.isfile(from_path) and from_path.endswith(".md"):
+            generate_page(from_path, template_path, dest_path)
+        elif os.path.isdir(from_path):
+            generate_pages_recursive(from_path, template_path, os.path.join(dest_dir_path, entry))
