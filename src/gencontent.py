02d7e99c2fd4789a0809f00edda0f0ee9c0bc654
Rafael S Lima <rafaelkra2@gmail.com>
2025-10-27 23:59:03 -0300

    deploy: changes to deploy on Github Pages

--- src/gencontent.py
+++ src/gencontent.py
@@ -8,7 +8,7 @@ def extract_title(markdown):
             return line[2:].strip()
     raise ValueError("No title found in markdown")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(basepath, from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     with open(from_path, "r", encoding="utf-8") as f:
@@ -19,7 +19,7 @@ def generate_page(from_path, template_path, dest_path):
 
     html_content = markdown_to_html_node(markdown).to_html()
     title = extract_title(markdown)
-    final_html = template.replace("{{ Title }}", title).replace("{{ Content }}", html_content)
+    final_html = template.replace("{{ Title }}", title).replace("{{ Content }}", html_content).replace("href=\"/\"", f"href=\"{basepath}\"").replace("src=\"/\"", f"src=\"{basepath}\"")
 
     dest_dir = os.path.dirname(dest_path)
     if dest_dir and not os.path.exists(dest_dir):
@@ -28,11 +28,11 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, "w", encoding="utf-8") as f:
         f.write(final_html)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(basepath, dir_path_content, template_path, dest_dir_path):
     for entry in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, entry)
         dest_path = os.path.join(dest_dir_path, entry.replace(".md", ".html"))
         if os.path.isfile(from_path) and from_path.endswith(".md"):
-            generate_page(from_path, template_path, dest_path)
-        elif os.path.isdir(from_path):
-            generate_pages_recursive(from_path, template_path, os.path.join(dest_dir_path, entry))
+            generate_page(basepath, from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(basepath, from_path, template_path, os.path.join(dest_dir_path, entry))
